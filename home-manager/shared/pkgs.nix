9a0cdd42c34df63af2ba81f0d48d4ff3a3264f44
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-02-26 05:24:37 -0500

    Github auth

--- home-manager/shared/pkgs.nix
+++ home-manager/shared/pkgs.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
 
   # Specify the desired packages to install in the user environment.
-  home.packages = with pkgs; [ direnv git-credential-manager compose2nix ];
+  home.packages = with pkgs; [
+    direnv
+    git-credential-manager
+    compose2nix
+    git-credential-oauth
+  ];
 }
